9d04674373bc813e7ec0bbacf2282c98f0c87dcf
naerim <naerim1119@gmail.com>
2022-01-11 20:15:49 +0900

    set stackview

---
+++ stackview/stackview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stackview
+//
+//  Created by 김내림 on 2022/01/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
