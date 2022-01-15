82d8408e7f430e445dc5309b7809c7e14dde8f97
naerim <naerim1119@gmail.com>
2022-01-15 15:48:27 +0900

    compositional layout

--- dynamic_tableview/dynamic_tableview/MyCollectionVC.swift
+++ dynamic_tableview/dynamic_tableview/MyCollectionVC.swift
@@ -31,6 +31,118 @@ class MyCollectionVC: UIViewController {
         let myCustomCollectionViewCellNib = UINib(nibName: String(describing: MyCustomCollectionViewCell.self), bundle: nil)
         // 가져온 닙파일로 콜렉션뷰에 셀로 등록
         self.myCollectionView.register(myCustomCollectionViewCellNib, forCellWithReuseIdentifier: String(describing: MyCustomCollectionViewCell.self))
+        
+        // 콜렉션뷰의 콜렉션뷰 레이아웃 설정
+        self.myCollectionView.collectionViewLayout = createCompositionalLayoutForFirst()
+    }
+    
+    @IBAction func onCollectionViewTypeChanged(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            self.myCollectionView.collectionViewLayout = createCompositionalLayoutForFirst()
+        case 1:
+            self.myCollectionView.collectionViewLayout = createCompositionalLayoutForSecond()
+        case 2:
+            self.myCollectionView.collectionViewLayout = createCompositionalLayoutForThird()
+        default:
+            break
+        }
+    }
+}
+
+// MARK: - 콜렉션뷰 콤포지셔널 레이아웃 관련
+extension MyCollectionVC {
+    
+    // 콤포지셔널 레이아웃 설정
+    fileprivate func createCompositionalLayoutForFirst() -> UICollectionViewLayout {
+        // 콤포지셔널 레이아웃 생성
+        let layout = UICollectionViewCompositionalLayout {
+            // 튜플의 묶음으로 들어옴. 반환 하는 것은 NSCollectionLayouttSection 콜렉션 레이아웃 섹션을 반환해야함
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            // 아이템에 대한 크기 - absolute: 고정값, estimated: 추측, fraction: 퍼센트
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            // 아이템 만들기
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            // 아이템 간의 간격 설정
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            
+            let groupHeight = NSCollectionLayoutDimension.fractionalWidth(1/3)
+            // 그룹 사이즈
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: groupHeight)
+            // 그룹 만들기
+//            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item, item, item])
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+            
+            // 섹션 만들기
+            let section = NSCollectionLayoutSection(group: group)
+            // 섹션에 대한 간격
+            //section.orthogonalScrollingBehavior = .groupPaging // 스크롤
+            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+            
+            return section
+            
+        }
+        return layout
+    }
+    
+    fileprivate func createCompositionalLayoutForSecond() -> UICollectionViewLayout {
+
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            // 아이템 만들기
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            // 아이템 간의 간격 설정
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            
+            let groupHeight = NSCollectionLayoutDimension.fractionalWidth(1/2)
+            // 그룹 사이즈
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: groupHeight)
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 2)
+            
+            // 섹션 만들기
+            let section = NSCollectionLayoutSection(group: group)
+            // 섹션에 대한 간격
+            //section.orthogonalScrollingBehavior = .groupPaging // 스크롤
+            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+            
+            return section
+            
+        }
+        return layout
+    }
+    
+    fileprivate func createCompositionalLayoutForThird() -> UICollectionViewLayout {
+        // 콤포지셔널 레이아웃 생성
+        let layout = UICollectionViewCompositionalLayout {
+            // 튜플의 묶음으로 들어옴. 반환 하는 것은 NSCollectionLayouttSection 콜렉션 레이아웃 섹션을 반환해야함
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            
+            // 아이템에 대한 크기 - absolute: 고정값, estimated: 추측, fraction: 퍼센트
+            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+            // 아이템 만들기
+            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+            // 아이템 간의 간격 설정
+            item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+            
+            let groupHeight = NSCollectionLayoutDimension.fractionalWidth(1/3)
+            // 그룹 사이즈
+            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: groupHeight)
+            // 그룹 만들기
+//            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item, item, item])
+            let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)
+            
+            // 섹션 만들기
+            let section = NSCollectionLayoutSection(group: group)
+            // 섹션에 대한 간격
+            //section.orthogonalScrollingBehavior = .groupPaging // 스크롤
+            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+            
+            return section
+            
+        }
+        return layout
     }
 }
 
