7c864c7e23cd7a7a89af85172c0d24203aba31e7
naerim <naerim1119@gmail.com>
2022-01-17 23:21:01 +0900

    login ui 구현

--- firebaseTest/firebaseTest/ViewController.swift
+++ firebaseTest/firebaseTest/ViewController.swift
@@ -9,9 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+        
+    @IBAction func loginBtn(_ sender: UIButton) {
+        guard let id = self.idTextField.text else { return
+        }
+        guard let pw = self.pwTextField.text else {
+            return
+        }
+        printuserInfo(id: id, pw: pw)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @objc fileprivate func printuserInfo(id:String, pw:String) {
+        print(id, pw)
     }
 
 
